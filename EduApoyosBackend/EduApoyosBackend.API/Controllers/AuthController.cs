d8d1fa2990a71dd5ba3c128340e1a4bc9fb887a7
Andrés Castañeda <andfcast@hotmail.com>
2026-07-20 12:47:38 -0500

    Definición estructura poryecto y creación controladores de API

---
+++ EduApoyosBackend/EduApoyosBackend.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EduApoyosBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {        
+
+        // POST api/<AuthController>
+        [HttpPost("Login")]
+        public void Login([FromBody] string value)
+        {
+        }
+        // POST api/<AuthController>
+        [HttpPost("Register")]
+        public void Register([FromBody] string value)
+        {
+        }        
+    }
+}
